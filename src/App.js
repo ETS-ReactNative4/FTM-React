45eeee64ffb2c1f4b7af558bb18327256097b428
atsai1220 <14171643+atsai1220@users.noreply.github.com>
2018-04-19 03:45:34 -0600

    formatted home page with filters

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+import { MuiThemeProvider, createMuiTheme} from 'material-ui/styles';
 import FTMAppBar from './ftmAppBar/FTMAppBar';
 import Home from './home/Home';
 import Recipe from './recipe/Recipe';
@@ -15,11 +15,10 @@ class App extends Component {
       <div className="app-container">
         <BrowserRouter>
           <MuiThemeProvider theme={theme}>
-
             <div className="app-bar">
-              <FTMAppBar/>
+              <FTMAppBar />
             </div>
-            <div className="content-area">
+            <div className="content-area" >
               <Route exact path="/" component={Home} />
               <Route path="/recipe" component={Recipe} />
               <Route path="/login" component={Login} />
