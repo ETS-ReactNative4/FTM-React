18c33f53c144f2e20c0d87f2b044894a900cb822
atsai1220 <14171643+atsai1220@users.noreply.github.com>
2018-12-02 22:34:43 -0700

    Added ClickAwayListener to buttons

--- src/home/FilterButton/FilterButton.js
+++ src/home/FilterButton/FilterButton.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, Menu, MenuItem } from '@material-ui/core';
+import { Button, Menu, MenuItem, ClickAwayListener } from '@material-ui/core';
 import { withStyles } from 'material-ui/styles';
 
 class FilterButton extends React.Component {
@@ -30,18 +30,22 @@ class FilterButton extends React.Component {
       </MenuItem>
     ));
     return (
-      <div style={{ width: '100%' }}>
-        <Button
-          aria-owns={open ? 'filter-button' : null}
-          aria-haspopup="true"
-          onClick={this.handleMenu}
-          color="primary"
-        >
-          {title}
-        </Button>
-        <Menu id="filter-button" anchorEl={anchorEl} open={open} onClose={this.handleChange}>
-          {listItems}
-        </Menu>
+      <div style={{ width: '100%', flex: '1' }}>
+        <ClickAwayListener onClickAway={this.handleClose}>
+          <Button
+            style={{ justifySelf: 'center' }}
+            aria-owns={open ? 'filter-button' : null}
+            aria-haspopup="true"
+            onClick={this.handleMenu}
+            color="primary"
+          >
+            {title}
+          </Button>
+
+          <Menu id="filter-button" anchorEl={anchorEl} open={open} onClose={this.handleChange}>
+            {listItems}
+          </Menu>
+        </ClickAwayListener>
       </div>
     );
   }
