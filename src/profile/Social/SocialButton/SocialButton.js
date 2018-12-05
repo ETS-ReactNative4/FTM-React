9b60d28c6b475aa8e3c078591bf6d98c832f2688
McKay Fenn <mckay.fenn@gmail.com>
2018-12-04 17:42:15 -0700

    remove unnecessary

--- src/profile/Social/SocialButton/SocialButton.js
+++ src/profile/Social/SocialButton/SocialButton.js
@@ -4,22 +4,10 @@ import { Card, Icon } from '@material-ui/core';
 import '../Social.css';
 
 class SocialButton extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      styleActive: false,
-    };
-  }
-
-  toggleClass() {
-    console.log('toggle class clicked');
-    const currentState = this.state.styleActive;
-    this.setState({ styleActive: !currentState });
-  }
 
   render() {
     return (
-      <Card className={this.props.classActive ? 'num-container btn-active' : 'num-container'} onClick={() => { this.props.delegateShowResults(this.props.delegateShow); this.toggleClass(); }}>
+      <Card className={this.props.classActive ? 'num-container btn-active' : 'num-container'} onClick={() => this.props.delegateShowResults(this.props.delegateShow)}>
         <Icon>{this.props.icon}</Icon>
         <span className="number">{this.props.number}</span><br />
         <span>{this.props.title}</span>
