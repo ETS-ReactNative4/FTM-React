f6e271bdb2a20ad16b245948437e878008ce4296
Bernard Cosgriff <bernardcosgriff@gmail.com>
2018-12-07 04:22:52 -0700

    Trying to get delete to work

--- src/home/SearchResult/SearchResult.js
+++ src/home/SearchResult/SearchResult.js
@@ -7,11 +7,9 @@ import {
   Avatar,
   CardHeader,
   CardMedia,
-  MenuList,
-  MenuItem,
-  ListItemIcon,
+  IconButton
 } from '@material-ui/core';
-import { Star, StarBorder } from '@material-ui/icons';
+import { Close } from '@material-ui/icons';
 import { Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './SearchResult.css';
@@ -22,26 +20,26 @@ const styles = {
     marginLeft: 10,
     marginRight: 10,
     marginBottom: 10,
-    cursor: 'pointer',
+    cursor: 'pointer'
   },
   media: {
     height: 0,
-    paddingTop: '75%', // 16:9
+    paddingTop: '75%' // 16:9
   },
   actions: {
-    display: 'flex',
+    display: 'flex'
   },
   avatar: {
     width: 30,
-    height: 30,
+    height: 30
   },
   title: {
     marginBottom: 5,
-    fontSize: 12,
+    fontSize: 12
   },
   pos: {
-    marginBottom: 12,
-  },
+    marginBottom: 12
+  }
 };
 class SearchResult extends React.Component {
   state = { expanded: false };
@@ -50,14 +48,12 @@ class SearchResult extends React.Component {
     this.setState(state => ({ expanded: !state.expanded }));
   };
 
-  truncateString = (des) => {
+  truncateString = des => {
     console.log(des);
   };
 
   render() {
-    const {
-      classes, name, r_id, description, images, rating,
-    } = this.props;
+    const { classes, name, r_id, description, images, rating } = this.props;
 
     return (
       <Route
@@ -68,19 +64,27 @@ class SearchResult extends React.Component {
               history.push(`/recipe/${r_id}`);
             }}
           >
-
-						<CardMedia className={classes.media} image={images[0]} />
-						<CardHeader
-							className="recipe-header"
-							avatar={
-								<Avatar
-									alt={name}
-									className={classes.avatar}
-									src="http://i65.tinypic.com/2rnvc7k.png"
-								/>
-							}
-							title={name}
-						/>
+            <CardMedia className={classes.media} image={images[0]} />
+            <CardHeader
+              className="recipe-header"
+              avatar={
+                <Avatar
+                  alt={name}
+                  className={classes.avatar}
+                  src="http://i65.tinypic.com/2rnvc7k.png"
+                />
+              }
+              action={
+                <IconButton
+                  onClick={e => {
+                    e.preventDefault();
+                  }}
+                >
+                  <Close />
+                </IconButton>
+              }
+              title={name}
+            />
             <CardContent style={{ padding: '5px' }}>
               <Typography className="recipe-description" variant="caption">
                 {description}
@@ -98,7 +102,7 @@ SearchResult.propTypes = {
   name: PropTypes.any.isRequired,
   r_id: PropTypes.any.isRequired,
   description: PropTypes.any.isRequired,
-  images: PropTypes.any.isRequired,
+  images: PropTypes.any.isRequired
 };
 
 export default withStyles(styles)(SearchResult);
